914607a5cde68cc7c159205453f2629ce40e1a02
Veronika Rusakova <110855896+niiiru@users.noreply.github.com>
2023-05-23 19:33:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Products from "../views/Products.vue";
+import PastOrders from "../views/PastOrders.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/products",
+    name: "Products",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Products,
+  },
+  {
+    path: "/past-orders",
+    name: "PastOrders",
+    component: PastOrders,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
